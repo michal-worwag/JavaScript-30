e96adceae078734e0e24b9d77b18f1780c55486c
Michał Worwąg <michalwo@gmail.com>
2018-12-04 18:00:00 +0100

    Update 18 challenge

--- 18-AddingUpTimesWithReduce/scripts.js
+++ 18-AddingUpTimesWithReduce/scripts.js
@@ -7,3 +7,12 @@ const seconds = timeNodes
     return (mins * 60) + secs;
   })
   .reduce((total, VidSeconds) => total + VidSeconds);
+
+  let secondsLeft = seconds;
+  const hours = Math.floor(secondsLeft / 3600);
+  secondsLeft = secondsLeft % 3600
+
+  const mins = Math.floor(secondsLeft / 60);
+  secondsLeft = secondsLeft % 60;
+
+  console.log(hours, mins, secondsLeft);
